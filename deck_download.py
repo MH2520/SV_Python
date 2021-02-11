b20ef132e4d42f762567982cb1fb4de68eff51a9
Yijun He <yh403@cam.ac.uk>
2021-02-11 11:50:27 +0800

    Batch download decks

---
+++ deck_download.py
@@ -0,0 +1,14 @@
+import openpyxl
+import deck_image
+
+if __name__ == '__main__':
+    sheet = openpyxl.load_workbook('decklist.xlsx')['Sheet1']
+    names = []
+    nums = []
+    links = []
+    for i in range(8):
+        for j in range(3):
+            names.append('2_'+sheet.cell(i+1, 1).value)
+            nums.append(j+1)
+            links.append(sheet.cell(i+1, j+2).value)
+    deck_image.batch_download_deck(names, nums, links)
