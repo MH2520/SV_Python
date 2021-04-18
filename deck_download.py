f86e82f1919e83ded56d3d034af97a1ab8c8bd1a
Yijun He <yh403@cam.ac.uk>
2021-04-19 02:37:20 +0800

    Small format change

--- deck_download.py
+++ deck_download.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         for j in range(3):
             names.append('4_'+sheet.cell(i+1, 1).value)
             nums.append(j+1)
-            links.append(sheet.cell(i+1, j+4).value)
+            links.append(sheet.cell(i+1, j+2).value)
     deck_image.batch_download_deck(names, nums, links)
